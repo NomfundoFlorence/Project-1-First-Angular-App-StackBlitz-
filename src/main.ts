409453dc16610a841360deb1e746be999ddc5101
NomfundoFlorence <113187521+NomfundoFlorence@users.noreply.github.com>
2023-06-20 21:27:56 +0200

    5 files modified, 4 added

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,21 @@
 */
 import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 
 bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
+  {
+    providers: [
+      provideProtractorTestingSupport(), provideRouter(routeConfig)
+    ]
+  }
+  ).catch(err => console.error(err));
 
 
 /*
 Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
+Use of this source code is governed by an MIT-style license thatimport { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 can be found in the LICENSE file at https://angular.io/license
 */
